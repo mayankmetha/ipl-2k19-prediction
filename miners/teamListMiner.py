9f8cbf1e2421db62ab4370cae3220273ebe41584
mayankmetha <mayankmethad@gmail.com>
2019-03-25 21:26:01 +0530

    added team player list

---
+++ miners/teamListMiner.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+base = "https://cricketapi.platform.iplt20.com/tournaments/10192/squads/"
+
+page = requests.get(base).text
+data = json.loads(page)
+for teams in data['squads']['ALL']:
+    name = teams['team']['abbreviation']
+    fout = open('../datasets/teamList/'+name,'w')
+    for p in teams['players']:
+        fout.write(p['fullName']+'\n')
+    fout.close()
