f04a4fe142896c1f9df10101aff60c30069003c3
mayankmetha <mayankmethad@gmail.com>
2019-03-31 14:50:38 +0530

    fixed ui

--- prediction/selectPlayers.py
+++ prediction/selectPlayers.py
@@ -2,12 +2,41 @@ from tkinter import *
 
 teams = ['CSK','DC','KXIP','KKR','MI','RR','RCB','SRH']
 team = []
+team1S = []
+team2S = []
 teamVar = None
 win = None
 team1L = None
 team2L = None
 team1 = None
 team2 = None
+team1SB = None
+team2SB = None
+root = None
+
+def main():
+    global root
+    global team1
+    global team2
+    global team1L
+    global team2L
+    root.geometry("500x500")
+    root.title("IPL Prediction")
+    teamText = Label(root,text="SELECT TEAMS")
+    teamText.pack()
+    teamText.place(relx=0.5,rely=0.025,anchor=CENTER)
+    team1 = Button(root,text="Team1",command=teamSelect)
+    team1.pack()
+    team1.place(relx=0.3,rely=0.075,anchor=CENTER)
+    team1L = Label(root,text="Team1:")
+    team1L.pack()
+    team1L.place(relx=0.60,rely=0.075,anchor=CENTER)
+    team2 = Button(root,text="Team2",command=teamSelect)
+    team2.pack()
+    team2.place(relx=0.3,rely=0.15,anchor=CENTER)
+    team2L = Label(root,text="Team2:")
+    team2L.pack()
+    team2L.place(relx=0.60,rely=0.15,anchor=CENTER)
 
 def teamSelect():
     global teamVar
@@ -43,6 +72,7 @@ def updateTeams():
         team1.configure(state=DISABLED)
         team1.pack()
         team1.place(relx=0.3,rely=0.075,anchor=CENTER)
+        team1Function()
     except:
         pass
     try:
@@ -53,26 +83,30 @@ def updateTeams():
         team2.configure(state=DISABLED)
         team2.pack()
         team2.place(relx=0.3,rely=0.15,anchor=CENTER)
+        team2Function()
     except:
         pass
 
-root = Tk()
-root.geometry("500x500")
-root.title("IPL Prediction")
-teamText = Label(root,text="SELECT TEAMS")
-teamText.pack()
-teamText.place(relx=0.5,rely=0.025,anchor=CENTER)
-team1 = Button(root,text="Team1",command=teamSelect)
-team1.pack()
-team1.place(relx=0.3,rely=0.075,anchor=CENTER)
-team1L = Label(root,text="Team1:")
-team1L.pack()
-team1L.place(relx=0.60,rely=0.075,anchor=CENTER)
-team2 = Button(root,text="Team2",command=teamSelect)
-team2.pack()
-team2.place(relx=0.3,rely=0.15,anchor=CENTER)
-team2L = Label(root,text="Team2:")
-team2L.pack()
-team2L.place(relx=0.60,rely=0.15,anchor=CENTER)
-root.mainloop()
+def team1Function():
+    global root
+    global team1SB
+    team1SelectionText = Label(root,text="SELECT PLAYING 11 FOR "+team[0])
+    team1SelectionText.pack()
+    team1SelectionText.place(relx=0.5,rely=0.215,anchor=CENTER)
+    team1SB = Button(root,text=team[0]+" PLAYING 11")
+    team1SB.pack()
+    team1SB.place(relx=0.5,rely=0.275,anchor=CENTER)
 
+def team2Function():
+    global root
+    global team2SB
+    team2SelectionText = Label(root,text="SELECT PLAYING 11 FOR "+team[1])
+    team2SelectionText.pack()
+    team2SelectionText.place(relx=0.5,rely=0.350,anchor=CENTER)
+    team2SB = Button(root,text=team[1]+" PLAYING 11")
+    team2SB.pack()
+    team2SB.place(relx=0.5,rely=0.415,anchor=CENTER)
+    
+root = Tk()
+main()
+root.mainloop()
