554550cc867e72629c9b9dc594992720f8e13550
mayankmetha <mayankmethad@gmail.com>
2019-04-01 21:01:07 +0530

    added team configuration options

--- prediction/playingSquad.py
+++ prediction/playingSquad.py
@@ -13,6 +13,8 @@ bowlerButton = None
 win = None
 lb = None
 b = None
+up = None
+down = None
 i = 0
 
 def preprocess(index):
@@ -46,18 +48,81 @@ def main(index):
     l2 = Label(root,text="Batting order")
     l2.pack()
     l2.place(relx=0.5,rely=0.075,anchor=CENTER)
-    battingOrderButton = Button(root,text="Select Batting Order",command=battingOrder)
+    battingOrderButton = Button(root,text="Select Batting Order",width=30,command=battingOrder)
     battingOrderButton.pack()
     battingOrderButton.place(relx=0.5,rely=0.125,anchor=CENTER)
     l3 = Label(root,text="Bowlers List")
     l3.pack()
     l3.place(relx=0.5,rely=0.200,anchor=CENTER)
-    bowlerButton = Button(root,text="Select Bowlers",command=bowlerSelection)
+    bowlerButton = Button(root,text="Select Bowlers",width=30,command=bowlerSelection)
     bowlerButton.pack()
     bowlerButton.place(relx=0.5,rely=0.250,anchor=CENTER)
 
 def battingOrder():
-    pass
+    global win
+    global lb
+    global teamList
+    global b
+    win = Toplevel()
+    win.wm_title("Batting Order")
+    lb = Listbox(win,height=len(teamList))
+    for _ in teamList:
+        lb.insert(END,_)
+    lb.grid(row=0,columnspan=2)
+    up = Button(win, text="↑",width=5,command=upClicked)
+    up.grid(row=1,column=0)
+    down = Button(win, text="↓",width=5,command=downClicked)
+    down.grid(row=1,column=1)
+    b = Button(win, text="OK",width=15,command=ok1Clicked)
+    b.grid(row=3,columnspan=2)
+
+def upClicked():
+    global lb
+    try:
+        idx = lb.curselection()
+        if not idx:
+            return
+        for pos in idx:
+            if pos == 0:
+                continue
+            text = lb.get(pos)
+            lb.delete(pos)
+            lb.insert(pos-1,text)
+            lb.selection_set(pos-1)
+    except:
+        pass
+
+def downClicked():
+    global lb
+    try:
+        idx = lb.curselection()
+        if not idx:
+            return
+        for pos in idx:
+            if pos == lb.size()-1:
+                continue
+            text = lb.get(pos)
+            lb.delete(pos)
+            lb.insert(pos+1,text)
+            lb.selection_set(pos+1)
+    except:
+        pass
+
+def ok1Clicked():
+    global i
+    global win
+    global lb
+    global battingOrderButton
+    global BattingOrder
+    global b
+    for _ in lb.get(0,END):
+        BattingOrder.append(_)
+    i += 1
+    battingOrderButton.configure(state=DISABLED)
+    battingOrderButton.pack()
+    battingOrderButton.place(relx=0.5,rely=0.125,anchor=CENTER)
+    goNext()
+    win.destroy()
 
 def bowlerSelection():
     global win
@@ -82,19 +147,27 @@ def ok2Clicked():
     global b
     Bowlers = [lb.get(idx) for idx in lb.curselection()]
     i += 1
-    bowlerButton.configure(state=DISABLED)
-    bowlerButton.pack()
-    bowlerButton.place(relx=0.5,rely=0.250,anchor=CENTER)
+    if len(Bowlers) == 0:
+        pass
+    else:
+        bowlerButton.configure(state=DISABLED)
+        bowlerButton.pack()
+        bowlerButton.place(relx=0.5,rely=0.250,anchor=CENTER)
+        goNext()
     win.destroy()
-    goNext()
 
 def goNext():
     global i
     global number
     global Bowlers
+    global BattingOrder
     if i != 2:
         return
-    fout = open(os.getcwd()+'/../out/prediction/team'+number+'Bowlers','w')
+    fout = open(os.getcwd()+'/../out/prediction/team'+str(number)+'BattingOrder','w')
+    for _ in BattingOrder:
+        fout.write(_+"\n")
+    fout.close()
+    fout = open(os.getcwd()+'/../out/prediction/team'+str(number)+'Bowlers','w')
     for _ in Bowlers:
         fout.write(_+"\n")
     fout.close()
--- prediction/start.sh
+++ prediction/start.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-python3 selectPlayers.py
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Clean Up\033[0m"
+rm ../out/prediction/* 1> /dev/null 2>&1
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Clean Up\033[0m\n"
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Select Team & Squad\033[0m"
+python3 selectPlayers.py 1> /dev/null 2>&1
 if [[ $? -ne 1 ]]
 then
-    echo "Error occured in selectPlayers.py"
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Select Team & Squad\033[0m\n"
     exit
 fi
-python3 playingSquad.py 0
-python3 playingSquad.py 1
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Select Team & Squad\033[0m\n"
+team0=`cat ../out/prediction/teams|head -1`
+team1=`cat ../out/prediction/teams|tail -1`
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team0\033[0m"
+python3 playingSquad.py 0 1>/dev/null 2>&1
+if [[ $? -ne 1 ]]
+then
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team0\033[0m\n"
+    exit
+fi
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team0\033[0m\n"
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m"
+python3 playingSquad.py 1 1>/dev/null 2>&1
+if [[ $? -ne 1 ]]
+then
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m\n"
+    exit
+fi
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m\n"
