bf379162537c23e735f814a44d0a679402df4f1e
mayankmetha <mayankmethad@gmail.com>
2019-04-03 20:01:51 +0530

    fixed bug

--- prediction/start.sh
+++ prediction/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #clean up
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Clean Up\033[0m"
-rm ../out/prediction/* 1> /dev/null 2>&1
+rm ../out/prediction/* > /dev/null 2>&1
 echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Clean Up\033[0m\n"
 #step 1
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Select Team & Squad\033[0m"
-python3 selectPlayers.py 1> /dev/null 2>&1
+python3 selectPlayers.py > /dev/null 2>&1
 if [[ $? -ne 1 ]]
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Select Team & Squad\033[0m\n"
@@ -15,7 +15,7 @@ echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m E
 #step 2
 team0=`cat ../out/prediction/teams|head -1`
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team0\033[0m"
-python3 playingSquad.py 0 1>/dev/null 2>&1
+python3 playingSquad.py 0 > /dev/null 2>&1
 if [[ $? -ne 1 ]]
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team0\033[0m\n"
@@ -25,7 +25,7 @@ echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m E
 #step 3
 team1=`cat ../out/prediction/teams|tail -1`
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m"
-python3 playingSquad.py 1 1>/dev/null 2>&1
+python3 playingSquad.py 1 > /dev/null 2>&1
 if [[ $? -ne 1 ]]
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m\n"
@@ -34,7 +34,7 @@ fi
 echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Configure $team1\033[0m\n"
 #step 4
 echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Toss\033[0m"
-python3 toss.py 1> /dev/null 2>&1
+python3 toss.py > /dev/null 2>&1
 if [[ $? -ne 1 ]]
 then
     echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Toss\033[0m\n"
