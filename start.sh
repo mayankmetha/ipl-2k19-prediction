2b0eff570e669dd0d72bb5e1c8fc03d1cf9f2a18
mayankmetha <mayankmethad@gmail.com>
2019-04-17 16:40:03 +0530

    fix start script

--- start.sh
+++ start.sh
@@ -57,6 +57,17 @@ then
 fi
 echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Mapping Cluster Data\033[0m\n"
 cd ..
+#addition of cluster data
+cd pvp
+echo -ne "\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Executing \033[0m\033[1;37m:\033[0m\033[1;36m Aggregating Cluster Data\033[0m"
+python3 summing.py > /dev/null 2>&1
+if [[ $? -ne 1 ]]
+then
+    echo -ne "\r\033[1;37m[\033[0m\033[1;31m✘\033[0m\033[1;37m]\033[0m\033[1;33m Failed    \033[0m\033[1;37m:\033[0m\033[1;36m Aggregating Cluster Data\033[0m\n"
+    exit
+fi
+echo -ne "\r\033[1;37m[\033[0m\033[1;32m✔\033[0m\033[1;37m]\033[0m\033[1;33m Executed  \033[0m\033[1;37m:\033[0m\033[1;36m Aggregating Cluster Data\033[0m\n"
+cd ..
 exit
 ## TODO: add prob averaging here
 #gui step 1
