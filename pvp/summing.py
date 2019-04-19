60619a764175f98eee255159aea2ecc01eed66ae
mayankmetha <mayankmethad@gmail.com>
2019-04-19 11:18:36 +0530

    fixed bug

--- pvp/summing.py
+++ pvp/summing.py
@@ -3,17 +3,17 @@ import os
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_csv(os.getcwd()+'/../out/pvp/dataset')
-columns=['batsmen','bowler','0s','1s','2s','3s','4s','5s','6s','balls','wickets']
-outFile = os.getcwd()+'/../out/pvp/aggregate'
+df1 = pd.read_csv(os.getcwd()+'/../out/pvp/batsmenMap')
+columns=['batsmen','bowler','delivery','0s','1s','2s','3s','4s','5s','6s']
+outFile = os.getcwd()+'/../out/pvp/batsmenRed'
 f = open(outFile,'w')
 f.write(",".join(columns)+"\n")
-
 a = df1.groupby('batsmen')
 batsmen = None
 for key1, item1 in a:
     batsmen = str(key1)
     bowler = None
+    delivery = None
     zero = None
     one = None
     two = None
@@ -21,21 +21,40 @@ for key1, item1 in a:
     four = None
     five = None
     six = None
+    b = a.get_group(key1).groupby(['bowler'])
+    for key2, item2 in b:
+        bowler = str(key2)
+        c = b.get_group(key2).groupby(['delivery'])
+        for key3, item3 in c:
+            delivery = str(key3)
+            zero = str(c.get_group(key3)['0s'].sum())
+            one = str(c.get_group(key3)['1s'].sum())
+            two = str(c.get_group(key3)['2s'].sum())
+            three = str(c.get_group(key3)['3s'].sum())
+            four = str(c.get_group(key3)['4s'].sum())
+            five = str(c.get_group(key3)['5s'].sum())
+            six = str(c.get_group(key3)['6s'].sum())
+            f.write(batsmen+","+bowler+","+delivery+","+zero+","+one+","+two+","+three+","+four+","+five+","+six+"\n")
+f.close()
+
+df2 = pd.read_csv(os.getcwd()+'/../out/pvp/bowlersMap')
+columns=['batsmen','bowler','balls','wickets']
+outFile = os.getcwd()+'/../out/pvp/bowlersRed'
+f = open(outFile,'w')
+f.write(",".join(columns)+"\n")
+a = df2.groupby('batsmen')
+batsmen = None
+for key1, item1 in a:
+    batsmen = str(key1)
+    bowler = None
     balls = None
     wickets = None
     b = a.get_group(key1).groupby(['bowler'])
     for key2, item2 in b:
         bowler = str(key2)
-        zero = str(b.get_group(key2)['0s'].sum())
-        one = str(b.get_group(key2)['1s'].sum())
-        two = str(b.get_group(key2)['2s'].sum())
-        three = str(b.get_group(key2)['3s'].sum())
-        four = str(b.get_group(key2)['4s'].sum())
-        five = str(b.get_group(key2)['5s'].sum())
-        six = str(b.get_group(key2)['6s'].sum())
         balls = str(b.get_group(key2)['balls'].sum())
         wickets = str(b.get_group(key2)['wickets'].sum())
-        f.write(batsmen+","+bowler+","+zero+","+one+","+two+","+three+","+four+","+five+","+six+","+balls+","+wickets+"\n")
-
+        f.write(batsmen+","+bowler+","+balls+","+wickets+"\n")
 f.close()
+
 exit(1)
